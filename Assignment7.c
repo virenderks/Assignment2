3cd7742dc55454cb5202e3b926eb91717989ec3a
veer sharma <115632380+virenderks@users.noreply.github.com>
2023-03-28 16:07:03 +0530

    Add files via upload

---
+++ Assignment7.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    int num;
+
+    printf("Enter a number: ");
+    scanf("%d", &num);
+
+    if (num & 1) {
+        printf("%d is odd", num);
+    } else {
+        printf("%d is even", num);
+    }
+
+    return 0;
+}
