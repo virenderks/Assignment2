e4035d007d3c1ff3d079d6feef00b2709cb21717
veer sharma <115632380+virenderks@users.noreply.github.com>
2023-03-28 16:09:36 +0530

    Add files via upload

---
+++ Assignment8.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main() {
+    printf("Size of int: %lu bytes\n", sizeof(int));
+    printf("Size of float: %lu bytes\n", sizeof(float));
+    printf("Size of char: %lu bytes\n", sizeof(char));
+    printf("Size of double: %lu bytes\n", sizeof(double));
+    return 0;
+}
