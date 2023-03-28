9ebf20fc370f9e76de7d544cf06359e93c5e194a
veer sharma <115632380+virenderks@users.noreply.github.com>
2023-03-28 15:58:55 +0530

    Add files via upload

---
+++ Assignment4.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+    int num1, num2;
+
+    printf("Enter two numbers: ");
+    scanf("%d%d", &num1, &num2);
+
+    // Swap values without using temporary variable
+    num1 = num1 + num2;
+    num2 = num1 - num2;
+    num1 = num1 - num2;
+
+    printf("After swapping, num1 = %d and num2 = %d", num1, num2);
+
+    return 0;
+}
